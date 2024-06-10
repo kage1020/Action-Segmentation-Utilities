a2b59344145983fc71b8c8e6a5b668aa7dcb4731
kage1020 <4624528@ed.tus.ac.jp>
2024-06-10 16:56:20 +0900

    add object log parse

--- logger/main.py
+++ logger/main.py
@@ -14,18 +14,20 @@ class Logger:
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
-    def info(self, message: str | dict | list):
+    def info(self, message: str | dict | list | object):
         self.logger.info(Color.GREEN + self.__parse(message) + Color.RESET)
 
-    def warn(self, message: str | dict | list):
+    def warn(self, message: str | dict | list | object):
         self.logger.warning(Color.YELLOW + self.__parse(message) + Color.RESET)
 
-    def error(self, message: str | dict | list):
+    def error(self, message: str | dict | list | object):
         self.logger.error(Color.RED + self.__parse(message) + Color.RESET)
 
-    def __parse(self, message: str | dict | list):
+    def __parse(self, message: str | dict | list | object):
         if isinstance(message, dict):
             return "\n".join([f"{k}: {v}" for k, v in message.items()])
         if isinstance(message, list):
             return "\n".join([str(m) for m in message])
+        if isinstance(message, object):
+            return str(message)
         return message
