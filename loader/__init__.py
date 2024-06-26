023d5fcedca4d62bc1862235d58d5a75f2f12317
kage1020 <4624528@ed.tus.ac.jp>
2024-06-26 17:29:17 +0900

    format by ruff

--- loader/__init__.py
+++ loader/__init__.py
@@ -1,1 +1,14 @@
-from .main import SaladsDataset, SaladsDataLoader, GTEADataset, GTEADataLoader, BreakfastDataset, BreakfastDataLoader, NissanDataset, NissanDataLoader
+from .main import (
+    SaladsDataset,
+    SaladsDataLoader,
+    GTEADataset,
+    GTEADataLoader,
+    BreakfastDataset,
+    BreakfastDataLoader,
+    NissanDataset,
+    NissanDataLoader,
+    get_mapping,
+    get_actions,
+    get_matching,
+)
+from .c2ftcn import C2FTCNBreakfastDataset, C2FTCNBreakfastDataLoader
