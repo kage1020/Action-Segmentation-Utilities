67e0a2e5baabff791523f1c4d3b5da3bf3bcc9a8
kage1020 <4624528@ed.tus.ac.jp>
2024-08-15 22:11:59 +0900

    update init_seed

--- base/main.py
+++ base/main.py
@@ -120,13 +120,15 @@ class Base:
 
     @staticmethod
     def init_seed(seed: int = 42):
+        torch.backends.cudnn.enabled = True
         torch.backends.cudnn.benchmark = False
         torch.backends.cudnn.deterministic = True
         random.seed(seed)
         np.random.seed(seed)
         torch.manual_seed(seed)
-        torch.cuda.manual_seed_all(seed)
-        torch.cuda.manual_seed(seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(seed)
+            torch.cuda.manual_seed(seed)
 
     @staticmethod
     def get_device(cuda: int | str = 0) -> torch.device:
