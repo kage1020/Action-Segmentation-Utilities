ffad663e6df3955e827766e479a0a2a7d2b19e7b
kage1020 <4624528@ed.tus.ac.jp>
2024-09-04 14:29:52 +0900

    add how to 100 m

--- loader/README.md
+++ loader/README.md
@@ -9,6 +9,7 @@ This module provides the data loading tools for action segmentation.
 - `BreakfastDataset/BreakfastDataLoader`: [Breakfast](https://serre-lab.clps.brown.edu/resource/breakfast-actions-dataset/)
 - `Assembly101Dataset/Assembly101DataLoader`: [Assembly101](https://assembly-101.github.io/)
 - `AnomalousToyAssemblyDataset/AnomalousToyAssemblyDataLoader`: [AnomalousToyAssembly](https://usa.honda-ri.com/ata)
+- `Howto100MDataset/Howto100MDataLoader`: [HowTo100M](https://www.di.ens.fr/willow/research/howto100m/)
 - `NissanDataset/NissanDataLoader`: Not available for public
 - `I3DDataset/I3DDataLoader`: [I3D](https://arxiv.org/abs/1705.07750)
 - `S3DDataset/S3DDataLoader`: [S3D](https://arxiv.org/pdf/1712.04851)
--- loader/main.py
+++ loader/main.py
@@ -216,6 +216,24 @@ class AnomalousToyAssemblyDataLoader(DataLoader):
         )
 
 
+class Howto100MDataset(BaseDataset):
+    def __init__(self, cfg: Config, train: bool = True):
+        super().__init__(cfg, "Howto100MDataset", train)
+
+
+class Howto100MDataLoader(DataLoader):
+    dataset: Howto100MDataset
+
+    def __init__(self, cfg: Config, train: bool = True, collate_fn=None):
+        dataset = Howto100MDataset(cfg, train)
+        super().__init__(
+            dataset=dataset,
+            batch_size=cfg.dataset.batch_size,
+            shuffle=cfg.dataset.shuffle,
+            collate_fn=collate_fn,
+        )
+
+
 class NissanDataset(BaseDataset):
     def __init__(self, cfg: Config, train: bool = True):
         super().__init__(cfg, "NissanDataset", train)
