d8ff5c5904a3c4d16f1e5231f8ad31a17313aec5
kage1020 <4624528@ed.tus.ac.jp>
2024-09-23 16:57:32 +0900

    Refactor image extraction and feature extraction process

--- extractor/main.py
+++ extractor/main.py
@@ -2,6 +2,7 @@ import os
 import ffmpeg
 import glob
 from pathlib import Path
+from tqdm import tqdm
 import numpy as np
 import torch
 import torch.nn as nn
@@ -26,7 +27,11 @@ class Extractor(Base):
         ), "video_dir or image_dir must be provided"
 
         self.out_dir = Path(out_dir)
-        video_paths = glob.glob(f"{video_dir}/*.mp4") if video_dir is not None else []
+        video_paths = (
+            sum([list(Path(video_dir).glob(f"**/*.{ext}")) for ext in ["mp4", "avi"]], [])
+            if video_dir is not None
+            else []
+        )
         video_paths.sort()
         self.video_paths = [Path(video_path) for video_path in video_paths]
         self.image_dir = image_dir
@@ -34,47 +39,60 @@ class Extractor(Base):
         os.makedirs(self.out_dir, exist_ok=True)
 
     def extract_features(self, model: nn.Module, model_name: str):
-        image_paths = sum(
-            [glob.glob(f"{self.image_dir}/*.{ext}") for ext in ["png", "jpg", "jpeg"]],
-            [],
-        )
-        if model_name == "i3d":
-            loader = DataLoader(
-                ImageBatch(image_paths, 15),
-                batch_size=1,
-                shuffle=False,
-                collate_fn=lambda x: x[0],
-                num_workers=torch.get_num_threads(),
-            )
-        elif model_name == "s3d":
-            loader = DataLoader(
-                ImageBatch(image_paths, 32),
-                batch_size=1,
-                shuffle=False,
-                collate_fn=lambda x: x[0],
-                num_workers=torch.get_num_threads(),
-            )
-        else:
-            raise ValueError(f"model_name {model_name} is not supported")
+        if self.image_dir is None:
+            return
 
-        model.eval()
-        with torch.no_grad():
-            features = []
-            for i, (rgb, flows) in enumerate(loader):
-                rgb = rgb.to(model.device)
-                flows = flows.to(model.device)
-                rgb_features = model(rgb)
-                flows_features = model(flows)
-                features.append(
-                    torch.cat([rgb_features, flows_features], dim=1).cpu().numpy()
+        image_dirs = Path(self.image_dir).glob("*")
+        for image_dir in tqdm(image_dirs, leave=False):
+            image_paths = sum(
+                [glob.glob(f"{image_dir}/*.{ext}") for ext in ["png", "jpg", "jpeg"]],
+                [],
+            )
+            image_paths.sort()
+            if model_name == "i3d":
+                loader = DataLoader(
+                    ImageBatch(image_paths, 15),
+                    batch_size=1,
+                    shuffle=False,
+                    collate_fn=lambda x: x[0],
+                    num_workers=torch.get_num_threads(),
                 )
+            elif model_name == "s3d":
+                loader = DataLoader(
+                    ImageBatch(image_paths, 32),
+                    batch_size=1,
+                    shuffle=False,
+                    collate_fn=lambda x: x[0],
+                    num_workers=torch.get_num_threads(),
+                )
+            else:
+                raise ValueError(f"model_name {model_name} is not supported")
 
-            features = np.concatenate(features, axis=0)
-            np.save(self.out_dir / "features.npy", features)
+            model.eval()
+            with torch.no_grad():
+                features = []
+                for i, (rgb, flows) in enumerate(loader):
+                    rgb = rgb.to(model.device)
+                    flows = flows.to(model.device)
+                    rgb_features = model(rgb)
+                    flows_features = model(flows)
+                    features.append(
+                        torch.cat([rgb_features, flows_features], dim=1).cpu().numpy()
+                    )
+
+                features = np.concatenate(features, axis=0)
+                np.save(self.out_dir / image_dir.stem / "features.npy", features)
 
     def extract_images(self):
-        for video_path in self.video_paths:
+        for video_path in tqdm(self.video_paths, leave=False):
             video_name = video_path.stem
-            out_dir = self.out_dir / video_name
+            out_dir = self.out_dir / video_path.parent.name / video_name
+
+            if out_dir.exists():
+                continue
+
             out_dir.mkdir(parents=True, exist_ok=True)
-            ffmpeg.input(str(video_path)).output(str(out_dir / "%08d.png")).run()
+
+            ffmpeg.input(str(video_path)).output(
+                str(out_dir / "%08d.png"), loglevel="quiet"
+            ).run()
