98a9996d353c8e138fffb022d95c2121edcad557
kage1020 <4624528@ed.tus.ac.jp>
2024-12-25 17:13:18 +0900

    add module trace logger

---
+++ logger/trace.py
@@ -0,0 +1,15 @@
+import sys
+
+def trace_module(frame, event, arg):
+    code = frame.f_code
+    module_name = code.co_filename
+    if (
+        event == "call"
+        and ".pyenv" not in module_name
+        and ".cache" not in module_name
+        and "<" not in module_name
+    ):
+        print(f"Module executed: {module_name}")
+    return trace_module
+
+sys.settrace(trace_module)
