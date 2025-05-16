fix image loading and normalization in ImageBatch class

loader/i3d.py
@@ -85,7 +85,7 @@ class ImageBatch(Dataset):
                 flow_tensor = transforms.functional.resize(flow_tensor, [224, 224])
                 self.flows[-1] = flow_tensor
             else:
-                next_img = Image.open(self.image_paths[next_index])
+                next_img = Image.open(self.image_paths[next_index + 1]).convert("RGB")
                 img_gray = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2GRAY)
                 next_img_gray = cv2.cvtColor(np.array(next_img), cv2.COLOR_RGB2GRAY)
                 flow: NDArray = opticalflow.calc(
@@ -93,8 +93,6 @@ class ImageBatch(Dataset):
                     next_img_gray.astype(np.uint8),
                     None,
                 )
-                self.flows[-1] = self.normalize(flow.clip(-20, 20))
-
         assert (
             len(self.images) == self.temporal_window + 1
         ), "length of chunk should be the same as temporal window + 1"
@@ -106,7 +104,9 @@ class ImageBatch(Dataset):
     def normalize(self, img: Image.Image | NDArray) -> torch.Tensor:
         _img: NDArray = cv2.resize(np.array(img), (224, 224)).astype(np.float32)
         _img += np.abs(np.min(_img))
-        _img = _img / 255.0 * 2 - 1
+        if np.max(_img) != 0:
+            _img /= np.max(_img)
+        _img = _img * 2 - 1
         return torch.from_numpy(_img).permute(2, 0, 1)
 
 
@@ -124,7 +124,7 @@ class I3DDataset(Dataset):
         self.image_dir = Path(image_dir)
         self.flow_dir = Path(flow_dir) if flow_dir is not None else None
         logger.info(f"Loading images from {self.image_dir} ...", end="")
-        image_dirs = get_dirs(image_dir, recursive=True)
+        image_dirs = get_dirs(self.image_dir, recursive=True)
         logger.info("Done")
         image_dirs.sort()
         self.image_dirs = image_dirs
@@ -133,7 +133,7 @@ class I3DDataset(Dataset):
         self.boundary_list: list[tuple[str, tuple[int, int]]] = []
         self.boundary_dict: dict[str, list[tuple[int, int]]] = {}
         if boundary_dir is not None:
-            boundary_list = list(get_boundaries(boundary_dir).items())
+            boundary_list = list(get_boundaries(Path(boundary_dir)).items())
             for video_name, boundaries in boundary_list:
                 self.boundary_list.extend([(video_name, b) for b in boundaries])
                 self.boundary_dict[video_name] = boundaries